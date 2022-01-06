Fixed orbital period calculation

src/calc/parallel/calc_flux.py
@@ -111,10 +111,9 @@ def main(**kwargs):
         mag_flux_u = np.sum(mf_u)
         mag_flux_l = np.sum(mf_l)
 
-        v_Kep0 = np.sqrt(mass/x1min)
-        Omega0 = v_Kep0/x1min
-        T0 = 2.*np.pi/Omega0
-        orbit_t = t/T0
+        r_ISCO = 6. # location of ISCO in PW potential
+        T_period = 2.*np.pi*sqrt(r_ISCO)*(r_ISCO - 2.)
+        orbit_t = t/T_period
         sim_t = float(t)
 
         with open(prob_dir + 'flux_with_time.csv', 'a', newline='') as f:

src/calc/parallel/calc_mass.py
@@ -110,10 +110,9 @@ def main(**kwargs):
                     mf_i.append(-dens[k,j,r_id_i] * v1[k,j,r_id_i] * (x1f[r_id_i])**2. * dOmega)
             mf_local.append(np.sum(mf_i))
 
-        v_Kep0 = np.sqrt(mass/x1min)
-        Omega0 = v_Kep0/x1min
-        T0 = 2.*np.pi/Omega0
-        orbit_t = t/T0
+        r_ISCO = 6 # location of ISCO in PW potential
+        T_period = 2.*np.pi*sqrt(r_ISCO)*(r_ISCO - 2.)
+        orbit_t = t/T_period
         sim_t = float(t)
 
         with open(prob_dir + 'mass_with_time.csv', 'a', newline='') as f:

src/calc/parallel/calc_qual.py
@@ -209,10 +209,9 @@ def main(**kwargs):
         Qp_l,Qp_h = st.t.interval(0.95, len(Q_phi)-1, loc=np.mean(Q_phi), scale=st.sem(Q_phi))
         Qp_av = np.mean(Q_phi)
 
-        v_Kep0 = np.sqrt(mass/x1min)
-        Omega0 = v_Kep0/x1min
-        T0 = 2.*np.pi/Omega0
-        orbit_t = t/T0
+        r_ISCO = 6 # location of ISCO in PW potential
+        T_period = 2.*np.pi*sqrt(r_ISCO)*(r_ISCO - 2.)
+        orbit_t = t/T_period
         sim_t = float(t)
 
         Qt_all = [Qt_l,Qt_av,Qt_h]

src/calc/parallel/calc_scale.py
@@ -105,10 +105,9 @@ def main(**kwargs):
         scale_h = np.sqrt(np.sum(h_up,axis=(0,1))/np.sum(h_down,axis=(0,1)))
         scale_h_av = np.average(scale_h,weights=dx1f)
 
-        v_Kep0 = np.sqrt(mass/x1min)
-        Omega0 = v_Kep0/x1min
-        T0 = 2.*np.pi/Omega0
-        orbit_t = t/T0
+        r_ISCO = 6 # location of ISCO in PW potential
+        T_period = 2.*np.pi*sqrt(r_ISCO)*(r_ISCO - 2.)
+        orbit_t = t/T_period
         sim_t = t
 
         with open(prob_dir + 'scale_with_time.csv', 'a', newline='') as f: