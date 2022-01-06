dc26d5f2e17ca1a63aba38f1635043f7ac68b5f9
Payton Rodman <per29@cam.ac.uk>
2022-01-06 13:05:34 +0000

    Commented out unused variable

--- src/plt/plt_beta.py
+++ src/plt/plt_beta.py
@@ -21,7 +21,7 @@ def main(**kwargs):
         next(csv_reader, None) # skip header
         for row in csv_reader:
             t = float(row[0])
-            t_orb = float(row[1])
+            #t_orb = float(row[1])
             b = float(row[2])
             time.append(t*5.)
             beta.append(b)
