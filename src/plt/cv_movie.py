fa94ee00c53749c2504847b560d55124e197e944
Payton Rodman <per29@cam.ac.uk>
2022-01-12 15:37:06 +0000

    Generalised for any image sequence

--- src/plt/cv_movie.py
+++ src/plt/cv_movie.py
@@ -15,7 +15,7 @@ import argparse
 def main(**kwargs):
     root_dir = '/Users/paytonrodman/athena-sim/'
     #root_dir = '/home/per29/rds/rds-accretion-zyNhkonJSR8/'
-    os.chdir(root_dir + args.prob_id + '/img/')
+    os.chdir(root_dir + args.prob_id + '/' + args.image_folder + '/')
 
     img_array = []
     for filename in sorted(glob.glob('*.png')):
@@ -36,15 +36,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create movie from stills')
     parser.add_argument('prob_id',
                         help='base name of the data being analysed, e.g. inflow_var or disk_base')
-    parser.add_argument('plot_id',
-                        help='base name of the sort of plot being animated, e.g. side_stream, side_plain, or above')
+    parser.add_argument('image_folder',
+                        help='name of the folder under prob_id, where stills are located')
     parser.add_argument('codec',
                         help='codec used to create movie, e.g. MJPG')
     parser.add_argument('output',
                         help='output filename and extension, e.g. high_res.avi')
     parser.add_argument('fps',
                         type=int,
-			default=5,
+			            default=5,
                         help='frames per second (default=5)')
     args = parser.parse_args()
 
