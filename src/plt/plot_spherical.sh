9a9a7dfa03f99136c99aa93baf87410a06b2dfeb
Payton Rodman <per29@cam.ac.uk>
2022-03-01 15:13:48 +0000

    Improved movie-making

--- src/plt/plot_spherical.sh
+++ src/plt/plot_spherical.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-#ROOTDIR=/Users/paytonrodman/athena-sim
-ROOTDIR=/home/per29/rds/rds-accretion-zyNhkonJSR8
-PROBLEM=high_res
+ROOTDIR=/Users/paytonrodman/athena-sim
+#ROOTDIR=/home/per29/rds/rds-accretion-zyNhkonJSR8
+PROBLEM=$1
+
+if [ $# -lt 3 ]
+  then
+    echo "Must specify (1) problem ID, (2) minimum time, (3) maximum time"
+    exit 1
+fi
 
 cd ${ROOTDIR}/${PROBLEM}/data/
 for FILE in ./${PROBLEM}.cons.*.athdf; do
@@ -12,20 +18,23 @@ for FILE in ./${PROBLEM}.cons.*.athdf; do
     NOZERO=0
   fi
 
-  REMAINDER=$(( $NOZERO % 10 ))
-  if (($REMAINDER==0)); then
-    if [[ ! -f ${ROOTDIR}/${PROBLEM}/img/${FILE//[^0-9]/}.png ]]; then
+  if [ "$NOZERO" -gt "$2" ] && [ "$NOZERO" -lt "$3" ]; then # if in range
+    REMAINDER=$(( $NOZERO % 10 ))
+    if (($REMAINDER==0)); then # do every 10th file
+      #if [[ ! -f ${ROOTDIR}/${PROBLEM}/img/${FILE//[^0-9]/}.png ]]; then # if image doesn't already exist
       python $ROOTDIR/athena-analysis/dependencies/plot_spherical.py \
       $FILE \
       dens \
       ../img/${FILE//[^0-9]/}.png \
-      --midplane \
       --colormap viridis \
-      --vmin 0.01 \
-      --vmax 0.1 \
+      --vmin 0.001 \
+      --vmax 0.05 \
       --logc \
+      --time \
+      --dpi 1200 \
       --stream Bcc \
-      --dpi 1200
+      --midplane
+      #fi
     fi
   fi
 done
