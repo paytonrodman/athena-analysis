a27a06421c2de46c807099d9fe56aff5869534c7
Payton Rodman <rodman.payton@gmail.com>
2023-06-01 12:25:56 +0100

    Fixed ME component addition

--- src/plt/plt_magE.py
+++ src/plt/plt_magE.py
@@ -9,6 +9,7 @@ sys.path.insert(0, '/Users/paytonrodman/athena-sim/athena-analysis/dependencies'
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
+import numpy as np
 
 # Athena++ modules
 import AAT
@@ -31,13 +32,19 @@ def main(**kwargs):
 
     t_lists = [[] for _ in range(n)]
     ME_lists = [[] for _ in range(n)]
+    ME_1_lists = [[] for _ in range(n)]
+    ME_2_lists = [[] for _ in range(n)]
+    ME_3_lists = [[] for _ in range(n)]
     for count,f in enumerate(args.file):
         df = pd.read_csv(f, skiprows=1, sep='\s+', escapechar='#')
+
+        # history file contains some duplicates due to restarts, remove them here
+        df.drop_duplicates(subset=[' [1]=time'], keep='last', inplace=True)
 
         ME_1 = df['[11]=1-ME'].to_list()
         ME_2 = df['[12]=2-ME'].to_list()
         ME_3 = df['[13]=3-ME'].to_list()
-        ME_tot = ME_1 + ME_2 + ME_3
+        ME_tot = [ME_1[i] + ME_2[i] + ME_3[i] for i in range(len(ME_1))]
 
         t = df[' [1]=time'].to_list()
 
@@ -45,33 +52,56 @@ def main(**kwargs):
             t_lists[count] = [AAT.calculate_orbit_time(ti) for ti in t]
         else:
             t_lists[count] = [ti/1e5 for ti in t] # convert time to units of 10^5 GM/c3
+        ME_1_lists[count]  = ME_1
+        ME_2_lists[count]  = ME_2
+        ME_3_lists[count]  = ME_3
         ME_lists[count] = ME_tot
 
     for ii in range(n):
-        t_lists[ii], ME_lists[ii] = zip(*sorted(zip(t_lists[ii], ME_lists[ii])))
+        t_lists[ii], ME_1_lists[ii], ME_2_lists[ii], ME_3_lists[ii], ME_lists[ii] = zip(*sorted(zip(t_lists[ii], ME_1_lists[ii], ME_2_lists[ii], ME_3_lists[ii], ME_lists[ii])))
 
+    #t = list(t_lists[0])
+    #print(t)
+    #print([i for i in t if i>2. and i<2.5])
+    #print(dict((x, indices(t, x)) for x in set(t) if t.count(x) > 1))
+    #print(jwbdjw)
     lw = 1.5
     if args.orbits:
         x_label = r'Time [ISCO Orbits]'
     else:
         x_label = r'Time [$10^5~GM/c^3$]'
-    y_label = r'Total Magnetic Energy'
+    y_label = r'Magnetic Energy'
 
-    _, ax1 = plt.subplots(nrows=1, ncols=1, constrained_layout=True, sharex=True)
+    _, axs = plt.subplots(nrows=1, ncols=n, constrained_layout=True, sharex=True)
     for ii in range(n):
-        if args.logy:
-            ax1.semilogy(t_lists[ii], ME_lists[ii], linewidth=lw, color=colors[ii], label=labels[ii])
+        if n>1:
+            ax = axs[ii]
         else:
-            ax1.plot(t_lists[ii], ME_lists[ii], linewidth=lw, color=colors[ii], label=labels[ii])
-    ax1.set_xlabel(x_label)#, x=0.5, y=-0.03)
-    ax1.set_ylabel(y_label)
+            ax = axs
+        if args.logy:
+            ax.semilogy(t_lists[ii], ME_1_lists[ii], linewidth=1, linestyle='dotted', color=colors[ii], label=r'$r$')
+            ax.semilogy(t_lists[ii], ME_2_lists[ii], linewidth=1, linestyle='dashed', color=colors[ii], label=r'$\theta$')
+            ax.semilogy(t_lists[ii], ME_3_lists[ii], linewidth=1, linestyle='dashdot', color=colors[ii], label=r'$\phi$')
+            ax.semilogy(t_lists[ii], ME_lists[ii], linewidth=lw, color=colors[ii], label='Total')
+            ax.set_ylim(bottom=0.01)
+        else:
+            ax.plot(t_lists[ii], ME_1_lists[ii], linewidth=1, linestyle='dotted', color=colors[ii], label=r'$r$')
+            ax.plot(t_lists[ii], ME_2_lists[ii], linewidth=1, linestyle='dashed', color=colors[ii], label=r'$\theta$')
+            ax.plot(t_lists[ii], ME_3_lists[ii], linewidth=1, linestyle='dashdot', color=colors[ii], label=r'$\phi$')
+            ax.plot(t_lists[ii], ME_lists[ii], linewidth=lw, color=colors[ii], label='Total')
 
-    leg = ax1.legend(loc='best')
-    for line in leg.get_lines():
-        line.set_linewidth(4.0)
+        ax.set_xlabel(x_label)#, x=0.5, y=-0.03)
+        ax.set_ylabel(y_label)
+        ax.set_title(labels[ii])
+
+        leg = ax.legend(loc='best')
+        #for line in leg.get_lines():
+        #    line.set_linewidth(2.0)
     plt.savefig(args.output, bbox_inches='tight')
     plt.close()
 
+def indices(lst, item):
+    return [i for i, x in enumerate(lst) if x == item]
 
 # Execute main function
 if __name__ == '__main__':
